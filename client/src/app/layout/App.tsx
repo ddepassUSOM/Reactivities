0166bb5573ddd022538ec88763967225a22a7ec1
Dwight A DePass <dwight@usomholdings.com>
2025-06-08 19:33:20 -0400

    install React.Query

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,23 +1,24 @@
-import { Box, Container, CssBaseline} from "@mui/material";
+import { Box, Container, CssBaseline, Typography} from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
 
-useEffect(() => {
- axios.get<Activity[]>('https://localhost:5001/api/activities')
- .then(response => setActivities(response.data))
-
- return () => {}
-}, [])
+  const {data: activities, isPending} = useQuery({
+    queryKey: ['activities'],
+    queryFn: async () => {
+      const response = await axios.get<Activity[]>('https://localhost:5001/api/activities');
+      return response.data;
+    }
+  })
 
 const handleSelectActivity = (id: string) => {
-  setSelectedActivity(activities.find(x => x.id === id));
+  setSelectedActivity(activities!.find(x => x.id === id));
 } 
 const handleCancelSelectActivity = () => {
   setSelectedActivity(undefined);
@@ -33,18 +34,20 @@ const handleFormClose = () => {
 }
 
 const handleSubmitForm = (activity: Activity)=>{
-  if(activity.id){
+ /*  if(activity.id){
       setActivities(activities.map(x => x.id === activity.id ? activity : x))
   }  else {
     const newActivity = {...activity, id: activities.length.toString()}
     setSelectedActivity(newActivity)
     setActivities([...activities, newActivity])
-  }
+  } */
+ console.log(activity);
   setEditMode(false)
 }
 
 const deleteActivity = (id: string) => {
-  setActivities(activities.filter(x => x.id !== id))  
+  /* setActivities(activities.filter(x => x.id !== id))   */
+  console.log(id);
 }
 
   return (
@@ -52,17 +55,22 @@ const deleteActivity = (id: string) => {
     <CssBaseline/>
     <NavBar openForm={handleOpenForm}/>
     <Container maxWidth='xl' sx={{mt: 3}}>
-      <ActivityDashboard 
-        activities={activities}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        selectedActivity={selectedActivity}
-        editMode={editMode}
-        openForm={handleOpenForm}
-        closeForm={handleFormClose}
-        submitForm={handleSubmitForm}
-        deleteActivity={deleteActivity}
+      {!activities || isPending ?(
+        <Typography>Loading...</Typography>
+      ) : (
+            <ActivityDashboard 
+            activities={activities}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            selectedActivity={selectedActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+            submitForm={handleSubmitForm}
+            deleteActivity={deleteActivity}
       />
+      )}
+     
     </Container>     
     </Box>
       
